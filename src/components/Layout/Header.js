88490e4fc6632eca2eacfd1b478d622f65bff0cc
Truong <gnourt2112000@gmail.com>
2022-06-07 22:37:15 +0700

    project init with layout and header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>React Meals</h1>
+                <h1>Meals Order</h1>
                 <HeaderCartButton></HeaderCartButton>
             </header>
             <div className={classes['main-image']}>
