84c51d040df8f5cf31d4374da843b131132a7453
tuantea <tuan1722000@gmail.com>
2022-06-08 09:46:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Header from "./components/Layout/Header";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
 import MealItem from "./components/Meals/MealItem/MealItem";
-import MealsSummary from "./components/Meals/MealsSummary";
-import AvailableMeals from "./components/Meals/AvailableMeals";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-	const [cartIsShown, setCartIsShown] = useState(false);
-	const handleShowCart = () => {
-		setCartIsShown(true);
-	};
-	const handleCloseCart = () => {
-		setCartIsShown(false);
-	};
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const handleShowCart = () => {
+    setCartIsShown(true);
+  };
+  const handleCloseCart = () => {
+    setCartIsShown(false);
+  };
 
-	return (
-		<CartProvider>
-			{cartIsShown && <Cart onCloseCart={handleCloseCart}></Cart>}
-			<Header onShowCart={handleShowCart}></Header>
-			<MealsSummary></MealsSummary>
-			<AvailableMeals></AvailableMeals>
-		</CartProvider>
-	);
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onCloseCart={handleCloseCart}></Cart>}
+      <Header onShowCart={handleShowCart}></Header>
+      <main>
+        <Meals></Meals>
+      </main>
+    </CartProvider>
+  );
 }
 
 export default App;
